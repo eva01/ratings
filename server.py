f38eb4e5aa843818c8cd886249fb1bf6c0f8b61d
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-01 17:56:51 -0700

    Add templates and routes

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 """Movie Ratings."""
 
 from jinja2 import StrictUndefined
-
-from flask import Flask, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
-
-from model import connect_to_db, db
+from flask import (Flask, render_template, redirect, request, flash,
+                   session)
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,9 +21,30 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    a = jsonify([1,3])
-    return a
+    return render_template("homepage.html")
 
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+
+@app.route('/login-form')
+def login():
+    """Prompts user to log in"""
+
+    return render_template('login_form.html')
+
+
+@app.route('/check-login', methods=['POST'])
+def check_login():
+    """Check if email in users table"""
+    # TODO: check email address against database
+
+    return redirect('/')
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
